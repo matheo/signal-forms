f0c3a250f59e66b4dbb481ff09b78747f43fadea
Mateo Tibaquira <nestormateo@gmail.com>
2026-06-06 16:10:56 -0500

    feat: add material card, button-toggle and icon-button

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
 import { QbExpression } from './query-builder';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.html',
   styleUrl: './app.scss',
-  imports: [QbExpression],
+  imports: [MatCardModule, QbExpression],
 })
 export class App {}
--- src/app/query-builder/components/qb-expression/qb-expression.ts
+++ src/app/query-builder/components/qb-expression/qb-expression.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
+import { MatIconButton } from '@angular/material/button';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { MatIcon } from '@angular/material/icon';
 import { QbCondition } from '../qb-condition/qb-condition';
 
 @Component({
   selector: 'app-qb-expression',
   templateUrl: './qb-expression.html',
   styleUrl: './qb-expression.scss',
-  imports: [QbCondition],
+  imports: [MatButtonToggleModule, MatIcon, MatIconButton, QbCondition],
 })
 export class QbExpression {}
