7f760e243759d3f5bdbcb848d07888fbc9a475c1
Mateo Tibaquira <nestormateo@gmail.com>
2026-06-07 23:42:10 -0500

    feat(query-builder): improve the UX, controls and look of the expressions and conditions

--- src/app/query-builder/components/qb-condition/qb-condition.ts
+++ src/app/query-builder/components/qb-condition/qb-condition.ts
@@ -1,17 +1,27 @@
-import { Component, input } from '@angular/core';
+import { Component, input, output } from '@angular/core';
 import { FormField, MaybeFieldTree } from '@angular/forms/signals';
+import { MatIconButton } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIcon } from '@angular/material/icon';
 import { MatInput } from '@angular/material/input';
-import { ConditionExpression, Expression } from '../../models';
+import { ConditionExpression } from '../../models';
 
 @Component({
   selector: 'app-qb-condition',
   templateUrl: './qb-condition.html',
   styleUrl: './qb-condition.scss',
-  imports: [FormField, MatFormFieldModule, MatInput],
+  imports: [
+    FormField,
+    MatFormFieldModule,
+    MatIcon,
+    MatIconButton,
+    MatInput,
+  ],
 })
 export class QbCondition {
   readonly form = input.required<
     MaybeFieldTree<ConditionExpression, number>
   >();
+
+  readonly doDelete = output<void>();
 }
--- src/app/query-builder/components/qb-expression/qb-expression.ts
+++ src/app/query-builder/components/qb-expression/qb-expression.ts
@@ -1,7 +1,9 @@
-import { Component, input } from '@angular/core';
+import { Component, input, output } from '@angular/core';
 import { MatIconButton } from '@angular/material/button';
 import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { MatCheckbox } from '@angular/material/checkbox';
 import { MatIcon } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
 import { FormField, MaybeFieldTree } from '@angular/forms/signals';
 import { ConditionExpression, Expression, LogicalExpression } from '../../models';
 import { QbCondition } from '../qb-condition/qb-condition';
@@ -10,11 +12,16 @@ import { QbCondition } from '../qb-condition/qb-condition';
   selector: 'app-qb-expression',
   templateUrl: './qb-expression.html',
   styleUrl: './qb-expression.scss',
+  host: {
+    class: 'block',
+  },
   imports: [
     FormField,
     MatButtonToggleModule,
+    MatCheckbox,
     MatIcon,
     MatIconButton,
+    MatMenuModule,
     QbCondition,
     QbExpression,
   ],
@@ -23,6 +30,10 @@ export class QbExpression {
   readonly form = input.required<
     MaybeFieldTree<LogicalExpression, string | number>
   >();
+
+  readonly addExpression = output<void>();
+  readonly addCondition = output<void>();
+  readonly doDelete = output<void>();
 
   isCondition(expression: MaybeFieldTree<Expression, number>): expression is MaybeFieldTree<ConditionExpression, number> {
     const value = expression().value();
